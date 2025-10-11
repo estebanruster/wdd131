5696c4fcd4987c242536643fed8a3eb4c744322d
estebanruster <emangascalva@byupathway.edu>
2025-10-10 19:28:08 -0600

    Lazyloading activity

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,5 @@
+let currentyear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentyear;
+
+let lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
